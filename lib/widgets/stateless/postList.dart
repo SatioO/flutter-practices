d329b82949396e8eb4fabf74b5add5aabdda7015
SatioO <vaibhav.satam29991@gmail.com>
2019-02-19 17:37:49 +0530

    fix(posts)

--- lib/widgets/stateless/postList.dart
+++ lib/widgets/stateless/postList.dart
@@ -3,16 +3,18 @@ import 'package:myapp/models/post.dart';
 
 class PostList extends StatelessWidget {
   final List<Post> posts;
-
   PostList({Key key, this.posts}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         itemCount: posts.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(title: Text(posts[index].title));
+          return ListTile(
+            title: Text(posts[index].title),
+            subtitle: Text(posts[index].body),
+          );
         });
   }
 }
